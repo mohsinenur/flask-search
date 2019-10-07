a17f3aebf660ebe8c58d648409c4273048a8852d
Mohsin <follow.mohsin@gmail.com>
2019-10-07 17:31:31 +0600

    getting search data

--- search/main/controllers.py
+++ search/main/controllers.py
@@ -12,7 +12,12 @@ def index():
 @main.route('/search', methods=['GET'])
 def search():
     if 'q' in request.args:
-        query = request.args.get("q")
-        return render_template('search/search.html', query=query)
+        q = request.args.get("q")
+        query = q.split()
+        url = ""
+        # get_result = requests.get(url).content
+        # result = json.loads(get_result)
+        # if user_detail['code'] == 200:
+        return render_template('search/search.html', q=q, query=query)
     else:
         return redirect('index')
